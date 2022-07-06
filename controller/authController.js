7c8544932a2f39d7041428c96c455dee23c5d2dd
Troy McDohl <troymcdohl@gmail.com>
2022-07-06 06:43:11 -0700

    'fsdf'

--- controller/authController.js
+++ controller/authController.js
@@ -86,6 +86,8 @@ exports.login = async (req, res, next) => {
 exports.logout = (req, res) => {
   res.cookie("jwt", "loggedout", {
     expires: new Date(Date.now() + 5 * 1000),
+    sameSite: "none",
+    secure: true,
   });
 
   res.status(200).json({ status: "success" });
